69c245b035cd0efe1c6489f9ce33ca39718da30d
J <hello.wjx@gmail.com>
2026-01-31 12:02:30 +0000

    fix: update default port to 5173

--- packages/vibedeckx/src/constants.ts
+++ packages/vibedeckx/src/constants.ts
@@ -3,4 +3,4 @@ import path from "path";
 
 export const VIBEDECKX_HOME = path.join(homedir(), ".vibedeckx");
 export const DB_PATH = path.join(VIBEDECKX_HOME, "data.sqlite");
-export const DEFAULT_PORT = 3000;
+export const DEFAULT_PORT = 5173;
